6d756e0b96befa3ba16e6d947a68719f4e0c3e80
kerwin-628 <648017848@qq.com>
2021-06-10 22:49:51 +0800

    每日一题(12) 6.10

---
+++ 20210610/20210610/20210610.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+	vector<vector<int>> findContinuousSequence(int target) {
+		vector<vector<int>> vec;
+		vector<int> res;
+		int sum = 0, limit = (target - 1) / 2;
+		for (int i = 1; i <= limit; ++i) {
+			for (int j = i;; ++j) {
+				sum += j;
+				if (sum > target) {
+					sum = 0;
+					break;
+				}
+				else if (sum == target) {
+					res.clear();
+					for (int k = i; k <= j; ++k) {
+						res.emplace_back(k);
+					}
+					vec.emplace_back(res);
+					sum = 0;
+					break;
+				}
+			}
+		}
+		return vec;
+	}
+};
+
+
+class Solution {
+public:
+	string reverseWords(string s) {
+		stack<char> word;
+		string result = "";
+		for (int i = s.size() - 1; i >= 0; --i)
+		{
+			if (s[i] != ' ')
+			{
+				word.push(s[i]);
+			}
+			if (s[i] == ' ' || i == 0)
+			{
+				bool flag = false;
+				while (word.empty() == false)
+				{
+					result.push_back(word.top());
+					word.pop();
+					flag = true;
+				}
+				if (flag)
+				{
+					result += " ";
+				}
+			}
+		}
+		return result.substr(0, result.size() - 1);
+	}
+};
